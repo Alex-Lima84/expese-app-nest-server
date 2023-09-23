7d6d186e640637448af329081c8617d782c94ef4
Alexandre Luiz <alexandre.cerutti@live.com>
2023-09-23 18:30:28 -0300

    database connection modification

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,10 @@ import { ExpensesModule } from './expenses/expenses.module';
 import { IncomesModule } from './incomes/incomes.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './typeorm/entities/user.entity';
+import 'dotenv/config';
+
+const { USERNAME, PASSWORD, HOST, DBPORT, DB } = process.env;
+const TYPE = 'postgres';
 
 @Module({
   imports: [
@@ -13,12 +17,12 @@ import { User } from './typeorm/entities/user.entity';
     ExpensesModule,
     IncomesModule,
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'admin',
-      database: 'expenseapi',
+      type: TYPE,
+      host: HOST,
+      port: parseInt(DBPORT, 10),
+      username: USERNAME,
+      password: PASSWORD,
+      database: DB,
       entities: [User],
       synchronize: true,
     }),
